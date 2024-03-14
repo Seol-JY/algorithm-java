9c68cfa275fd7ebcdf637f2aa68e94001b6c55f2
Seol_JY <wlsdud5654@gmail.com>
2024-03-14 10:44:19 +0900

    ✨ solved boj/2193

---
+++ boj/p2193/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.*;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+/*
+    solved BAEKJOON #2193 이친수
+    url : https://www.acmicpc.net/problem/2193
+*/
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        long[][] dp = new long[n + 1][2];
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
+            dp[i][1] = dp[i - 1][0];
+        }
+
+        bw.append(String.valueOf(dp[n][0] + dp[n][1]));
+        bw.flush();
+        bw.close();
+    }
+}
